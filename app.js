fbb9b06feed102591b07c2f4e28327e8d5542769
yaddysantos <katysantos424@gmail.com>
2022-06-30 08:00:11 -0500

    Conexion BD + adquirir informacion de cada producto

--- app.js
+++ app.js
@@ -8,13 +8,26 @@ import mysql from "mysql"
 //     })
 
 
-var paginas = 5;
+var paginas = 1;
 var CantidadItems = 0;
 var Contador = 1;
 var url = "";
 
+//conexion base de datos
+// var mysqlconection = mysql.createConnection({
+//     host: "http://mysql-repository.mysql.database.azure.com/",
+//     port: 3306,
+//     database: "productos_meli",
+//     user: "administrador",
+//     password:"%uTVtNe7"
+// });
+// mysqlconection.connect();
+
+//Valores
+
 //Recorrer las (5) paginas 
 for (let index = 1; index <= paginas; index++) {
+
     if (index > 1) { //Si es >1 quiere decir que esta en la siguiente pagina, se anexa en la url lo faltante
         Contador += 50;
         url = "_Desde_" + Contador + "_NoIndex_True"
@@ -25,20 +38,19 @@ for (let index = 1; index <= paginas; index++) {
     //buscar los elementos por la clase
     const listaItems = $(".ui-search-layout__item")
     CantidadItems += listaItems.length
-    
+
+    for (let index = 0; index < listaItems.length; index++) {
+        const DetalleProducto = $(".ui-search-item__title").get(index).children[0].data
+        //const PrecioProducto = Padre(".price-tag-fraction").get(index).text()
+        const startFull = $(".ui-search-reviews__ratings .ui-search-icon--star-full").get(0).children.length;
+        const startHalf = $(".ui-search-reviews__ratings .ui-search-icon--star-half").get(0).children.length * 0.5;
+        const Ranking = startFull + startHalf;
+        console.log(DetalleProducto +"  "+ Ranking)
+    }
 }
-console.log("Cantidad productos = " + CantidadItems + "\n" 
-            +"Contador paginas = " + Contador + "\n") // contarlos
+//console.log("Cantidad productos = " + CantidadItems + "\n"
+   // + "Contador paginas = " + Contador + "\n") // contarlos
 
 
-//conexion base de datos
-var mysqlconection = mysql.createConnection({
-    host: "localhost",
-    port: 8080,
-    database: "",
-    user: "",
-    password:""
-});
-mysqlconection.connect();
 
-mysqlconection.query("insert into values()")
+//mysqlconection.query("insert into PRODUCTO (detalle, precio, tipo_envio, ranking, disponibilidad) values()")

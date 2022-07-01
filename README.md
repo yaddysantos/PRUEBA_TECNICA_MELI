76dcaa77efa3affee6d918ef9f26a9c5090983c0
yaddysantos <katysantos424@gmail.com>
2022-06-30 21:14:06 -0500

    Creacion conexion e insercion informacion a la BD

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# prueba tecnica mercado libre
-para ejecutar el programa
+# Prueba técnica mercado libre
+En este proceso se lleva a cabo como Scrapear un Website con Node.js
+
+1. Se lleva a cabo la creación del proyecto con la siguiente línea de comando en la terminal. (Se crea por defecto un archivo llamado package.json ya que aquí iran las dependencias que iran del proyecto, y así estará en el repositorio de github).
 ```
-npm run start
+npm init -y
 ```
+2. Ahora, se requiere instalar Cheerio que es una herramienta rápida, flexible y facíl para analizar HTML, así mismo Axios, ya que este es un cliente HTTP que nos permitira hacer peticiones a un servidor basados en promesas para node.js
+```
+npm i axios cheerio
+```
+3. Y por último instalamos dependencia de MySql para poder hacer la conexión e inserción de los datos.
+```
+npm i mysql
+```
+4. Empezamos a inspeccionar la estructura HTML de la pagina web que se va a extraer los datos.
+
+PASOS
+* Importamos estas dependencias creadas
+* Se tiene alojada la base de datos remotamente en un servidor, y luego se crea la conexión (Linea 11 y 18). 
+* La idea es consultar los productos de las 5 primeras páginas del siguiente link https://listado.mercadolibre.com.ar/celular-smarphones, por medio de axios.get() se anexa dicho link y el resultado se carga con cheerio. (Linea 28, 29)
+*  Se adquiere la clase padre que contendrá todos los hijos, en este caso la lista de los productos en base a la búsqueda de celular smartphone (Linea 31, 32). Tener en cuenta que para navegar en las 5 primeras páginas se modificaría la url (Linea 25) 
+*  Luego se recorre cada producto de la página para adquirir la información y ser guardardad en nuestra base de datos. (Linea 35-49).
+* Y por último se cierra la conexión y el proceso.
--- app.js
+++ app.js
@@ -2,28 +2,23 @@ import axios from "axios"
 import cheerio from "cheerio"
 import mysql from "mysql"
 
-// axios.get("https://listado.mercadolibre.com.co/listado-celular")
-//     .then((res)=>{
-//         res.data
-//     })
-
-
-var paginas = 1;
+var paginas = 5;
 var CantidadItems = 0;
 var Contador = 1;
 var url = "";
 
-//conexion base de datos
-// var mysqlconection = mysql.createConnection({
-//     host: "http://mysql-repository.mysql.database.azure.com/",
-//     port: 3306,
-//     database: "productos_meli",
-//     user: "invitado",
-//     password:"invitado1234"
-// });
-// mysqlconection.connect();
-
-//Valores
+//conexion base de datos Azure MySQL, remotamente
+var mysqlconection = mysql.createConnection({
+    host: "mysql-repository.mysql.database.azure.com",
+    port: 3306,
+    database: "productos_meli",
+    user: "invitado",
+    password: "invitado1234"
+});
+mysqlconection.connect(null, (err) => {
+    if (err)
+        console.log(err)
+});
 
 //Recorrer las (5) paginas 
 for (let page = 1; page <= paginas; page++) {
@@ -35,29 +30,30 @@ for (let page = 1; page <= paginas; page++) {
     //obtener la pagina y la renderizada
     var { data } = await axios.get("https://listado.mercadolibre.com.ar/celular-smarphones" + url)
     const $ = cheerio.load(data)
-    //buscar los elementos por la clase
+    //Buscar los elementos por la clase
     const listaItems = $(".ui-search-layout__item")
     CantidadItems += listaItems.length
 
     for (let i = 0; i < listaItems.length; i++) {
         var padreItem = cheerio.load(listaItems.get(i))
         const DetalleProducto = padreItem(".ui-search-item__title").text()
-        console.log(DetalleProducto);
         const PrecioProducto = padreItem(".price-tag-fraction").text()
-        
-        console.log("indice i="+i+" /// "+(padreItem != undefined));
-        var Ranking = 0, startFull = 0, startHalf =0;
+        const DctoProducto = padreItem(".ui-search-price__second-line .price-tag-fraction").text()
+        const Dcto = padreItem(".ui-search-price__discount").text()
+        const VendedorProducto = padreItem(".ui-search-official-store-label").text()
+
+        var Ranking = 0, startFull = 0, startHalf = 0;
         if (padreItem != undefined) {
             startFull = padreItem(".ui-search-icon--star-full").length;
             startHalf = padreItem(".ui-search-icon--star-half").length * 0.5;
         }
         Ranking += startFull + startHalf;
-        console.log(Ranking);
+        //Insercion de la información requerida a la BD
+        mysqlconection.query(`insert into PRODUCTOS values ('${VendedorProducto}', '${DetalleProducto}', '${PrecioProducto}', '${Dcto}', '${DctoProducto}', '${Ranking}', ${page})`);
     }
+    console.log("Cantidad productos = " + CantidadItems)
 }
-//console.log("Cantidad productos = " + CantidadItems + "\n"
-   // + "Contador paginas = " + Contador + "\n") // contarlos
 
-
-
-//mysqlconection.query("insert into PRODUCTO (detalle, precio, tipo_envio, ranking, disponibilidad) values()")
+console.log("Proceso guardado XD.")
+//Cerrar el proceso del MySql
+mysqlconection.end();
